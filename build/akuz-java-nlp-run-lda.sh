d1325dce3ee155cc776f141fb4d5a42e443932d9
akuz <for.akuz@gmail.com>
2014-01-28 19:47:02 +0000

    Updated README to include topics configuration notes

--- build/akuz-java-nlp-run-lda.sh
+++ build/akuz-java-nlp-run-lda.sh
@@ -5,11 +5,7 @@ echo START: akuz-java-nlp-run-lda...
 java -Xms256m -Xmx4g \
         -Dfile.encoding=UTF-8 \
         -jar ./akuz-java-nlp-run-lda-0.0.1.jar \
-<<<<<<< HEAD
-        --inputDir ./input/news10k \
-=======
         -inputDir ./input/news_10k \
->>>>>>> f7e38be02f86edd2504e793817205110198d78f0
         -outputDir ./output \
         -topicsConfigFile ./topics_config.txt \
         -stopWordsFile ./stop_words.txt \
