9c219fa99e0b1b2c4494b3328eede0812c069699
akuz <for.akuz@gmail.com>
2014-08-03 10:39:13 +0100

    Addeded final to arguments

--- akuz-core/src/me/akuz/core/Rounding.java
+++ akuz-core/src/me/akuz/core/Rounding.java
@@ -2,8 +2,8 @@ package me.akuz.core;
 
 public final class Rounding {
 	
-	public static final double round(double value, int decimalPlaces) {
-		double normalizer = Math.pow(10, decimalPlaces);
+	public static final double round(final double value, final int decimalPlaces) {
+		final double normalizer = Math.pow(10, decimalPlaces);
 		return Math.round(value * normalizer) / normalizer;
 	}
 
