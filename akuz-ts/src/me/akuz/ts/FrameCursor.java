fb51ac716ece422b28ccc6f9de32e034b08af308
A Kuz <for.akuz@gmail.com>
2014-08-10 19:34:21 +0100

    Added documentation.

--- akuz-ts/src/me/akuz/ts/FrameCursor.java
+++ akuz-ts/src/me/akuz/ts/FrameCursor.java
@@ -4,18 +4,30 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Frame cursor interface provides information
- * about current time, keys, and items within
- * an iterator on a frame.
+ * {@link Frame} cursor provides information
+ * about keys and items in a frame at a
+ * particular (current) point in time.
  * 
  */
 public interface FrameCursor<K, T extends Comparable<T>> {
 	
+	/**
+	 * Get current time.
+	 */
 	T getCurrTime();
 	
+	/**
+	 * Get keys within cursor.
+	 */
 	List<K> getKeys();
 	
+	/**
+	 * Get current item by key.
+	 */
 	TItem<T> getCurrItem(K key);
 
+	/**
+	 * Get map of keys to current items.
+	 */
 	Map<K, TItem<T>> getCurrItems();
 }
--- akuz-ts/src/me/akuz/ts/SeqCursor.java
+++ akuz-ts/src/me/akuz/ts/SeqCursor.java
@@ -1,9 +1,22 @@
 package me.akuz.ts;
 
+/**
+ * 
+ * {@link Seq} cursor provides information
+ * about the item, if any, in a sequence at a
+ * particular (current) point in time.
+ * 
+ */
 public interface SeqCursor<T extends Comparable<T>> {
 
+	/**
+	 * Get current time.
+	 */
 	T getCurrTime();
 	
+	/**
+	 * Get current item, if any.
+	 */
 	TItem<T> getCurrItem();
 	
 }
