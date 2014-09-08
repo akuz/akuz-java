6fcc4d85952f2d758248c932f776b5ffbb817783
akuz <for.akuz@gmail.com>
2014-09-08 21:36:42 +0100

    Added test for MovAvgTDateTimeFilter.

--- akuz-ts/test/me/akuz/ts/filters/stats/MovAvgTDateTimeFilterTest.java
+++ akuz-ts/test/me/akuz/ts/filters/stats/MovAvgTDateTimeFilterTest.java
@@ -1,70 +1,69 @@
 package me.akuz.ts.filters.stats;
 
-import me.akuz.core.TDateTime;
-import me.akuz.core.TDuration;
+import me.akuz.core.TDate;
 import me.akuz.ts.Seq;
 import me.akuz.ts.SeqFilter;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MovAvgTWeekdaysFilterTest {
+public class MovAvgTDateTimeFilterTest {
 
 	@Test
 	public void testSimple2() {
 		
-		Seq<TDateTime> seq = new Seq<>();
+		Seq<TDate> seq = new Seq<>();
 		
-		seq.add(new TDateTime(2013, 1, 2),  2.5);
-		seq.add(new TDateTime(2013, 1, 3),  1.5);
-		seq.add(new TDateTime(2013, 1, 5), -1.0);
-		seq.add(new TDateTime(2013, 2, 7),  1.5);
-		seq.add(new TDateTime(2013, 2, 9), 0.5);
+		seq.add(new TDate(2013, 1, 2),  2.5);
+		seq.add(new TDate(2013, 1, 3),  1.5);
+		seq.add(new TDate(2013, 1, 7), -1.0);
+		seq.add(new TDate(2013, 2, 7),  1.5);
+		seq.add(new TDate(2013, 2, 11), 0.5);
 		
-		SeqFilter<TDateTime> filter
+		SeqFilter<TDate> filter
 			= new SeqFilter<>(seq.iterator())
-				.addFilter(new MovAvgTDateTimeFilter(2, TDuration.fromDays(2.01)));
+				.addFilter(new MovAvgTWeekdaysFilter(2, 2));
 		
-		filter.moveToTime(new TDateTime(2013, 1, 1));
+		filter.moveToTime(new TDate(2013, 1, 1));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 2));
+		filter.moveToTime(new TDate(2013, 1, 2));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 3));
+		filter.moveToTime(new TDate(2013, 1, 3));
 		Assert.assertEquals(2.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 4));
+		filter.moveToTime(new TDate(2013, 1, 4));
 		Assert.assertEquals(2.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 5));
+		filter.moveToTime(new TDate(2013, 1, 7));
 		Assert.assertEquals(0.25, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 6));
+		filter.moveToTime(new TDate(2013, 1, 8));
 		Assert.assertEquals(0.25, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 7));
+		filter.moveToTime(new TDate(2013, 1, 9));
 		Assert.assertEquals(0.25, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 8));
+		filter.moveToTime(new TDate(2013, 1, 10));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 2, 6));
+		filter.moveToTime(new TDate(2013, 2, 6));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 2, 7));
+		filter.moveToTime(new TDate(2013, 2, 7));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 2, 9));
+		filter.moveToTime(new TDate(2013, 2, 11));
 		Assert.assertEquals(1.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 2, 10));
+		filter.moveToTime(new TDate(2013, 2, 12));
 		Assert.assertEquals(1.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 2, 11));
+		filter.moveToTime(new TDate(2013, 2, 13));
 		Assert.assertEquals(1.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 2, 12));
+		filter.moveToTime(new TDate(2013, 2, 14));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
 	}
@@ -72,42 +71,42 @@ public class MovAvgTWeekdaysFilterTest {
 	@Test
 	public void testSimple3() {
 		
-		Seq<TDateTime> seq = new Seq<>();
+		Seq<TDate> seq = new Seq<>();
 		
-		seq.add(new TDateTime(2013, 1, 2),  2.5);
-		seq.add(new TDateTime(2013, 1, 3),  1.5);
-		seq.add(new TDateTime(2013, 1, 5), -1.0);
-		seq.add(new TDateTime(2013, 1, 6), -0.5);
+		seq.add(new TDate(2013, 1, 2),  2.5);
+		seq.add(new TDate(2013, 1, 3),  1.5);
+		seq.add(new TDate(2013, 1, 7), -1.0);
+		seq.add(new TDate(2013, 1, 8), -0.5);
 		
-		SeqFilter<TDateTime> filter
+		SeqFilter<TDate> filter
 			= new SeqFilter<>(seq.iterator())
-				.addFilter(new MovAvgTDateTimeFilter(3, TDuration.fromDays(2.01)));
+				.addFilter(new MovAvgTWeekdaysFilter(3, 2));
 		
-		filter.moveToTime(new TDateTime(2013, 1, 1));
+		filter.moveToTime(new TDate(2013, 1, 1));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 2));
+		filter.moveToTime(new TDate(2013, 1, 2));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 3));
+		filter.moveToTime(new TDate(2013, 1, 3));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 4));
+		filter.moveToTime(new TDate(2013, 1, 4));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
-		filter.moveToTime(new TDateTime(2013, 1, 5));
+		filter.moveToTime(new TDate(2013, 1, 7));
 		Assert.assertEquals(1.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 6));
+		filter.moveToTime(new TDate(2013, 1, 8));
 		Assert.assertEquals(0.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 7));
+		filter.moveToTime(new TDate(2013, 1, 9));
 		Assert.assertEquals(0.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 8));
+		filter.moveToTime(new TDate(2013, 1, 10));
 		Assert.assertEquals(0.0, filter.getCurrItem().getDouble(), 0.00000001);
 
-		filter.moveToTime(new TDateTime(2013, 1, 9));
+		filter.moveToTime(new TDate(2013, 1, 11));
 		Assert.assertTrue(Double.isNaN(filter.getCurrItem().getDouble()));
 
 	}
