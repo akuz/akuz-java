494a514b61fd9d4307435b6acce6da54ceafd78f
A Kuz <for.akuz@gmail.com>
2016-01-14 19:25:49 +0000

    Optimizing DDP.

--- akuz-mnist-digits/src/me/akuz/mnist/digits/visor/DDP.java
+++ akuz-mnist-digits/src/me/akuz/mnist/digits/visor/DDP.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+import me.akuz.core.math.GammaFunction;
 import me.akuz.ml.tensors.DenseTensor;
 import me.akuz.ml.tensors.Location;
 import me.akuz.ml.tensors.Shape;
 import me.akuz.ml.tensors.Tensor;
 import me.akuz.ml.tensors.TensorIterator;
-import me.akuz.ml.tensors.ViewTensor;
 
 /**
  * Discrete Dirichlet Process(es) along the last 
@@ -18,21 +18,24 @@ import me.akuz.ml.tensors.ViewTensor;
  */
 public final class DDP {
 	
+	final Shape _shape;
 	final int _ndim;
 	final int _lastDim;
 	final int _lastDimSize;
 	final int _lastDimLastIdx;
 	
 	final Tensor _base;
-	final Tensor _observed;
-	final Tensor _observedMass;
+	final double _baseMass;
+	final Tensor _obs;
+	final Tensor _obsMass;
+	final double _maxObsMass;
 
 	public DDP(
 			final Shape shape, 
 			final double baseInit,
 			final double baseNoise,
-			final double baseDistMass,
-			final double maxObservedMass) {
+			final double baseMass,
+			final double maxObsMass) {
 		
 		if (shape == null) {
 			throw new NullPointerException("shape");
@@ -43,22 +46,25 @@ public final class DDP {
 		if (baseNoise < 0.0) {
 			throw new IllegalArgumentException("baseInit must be >= 0, got " + baseNoise);
 		}
-		if (baseDistMass <= 0.0) {
-			throw new IllegalArgumentException("baseDistMass must be > 0, got " + baseDistMass);
+		if (baseMass <= 0.0) {
+			throw new IllegalArgumentException("baseMass must be > 0, got " + baseMass);
 		}
-		if (maxObservedMass <= 0.0) {
-			throw new IllegalArgumentException("maxObservedMass must be > 0, got " + maxObservedMass);
+		if (maxObsMass <= 0.0) {
+			throw new IllegalArgumentException("maxObservedMass must be > 0, got " + maxObsMass);
 		}
 		
 		// initialize base discrete distributions
 		// using the provided baseInit and baseNoise 
 		// parameters, and normalizing the last dimension:
 		//
+		_shape = shape;
 		_ndim = shape.ndim;
 		_lastDim = shape.ndim-1;
 		_lastDimSize = shape.sizes[_lastDim];
 		_lastDimLastIdx = _lastDimSize-1;
 		_base = new DenseTensor(shape);
+		_baseMass = baseMass;
+		_maxObsMass = maxObsMass;
 		final TensorIterator it = new TensorIterator(_base.shape);
 		final Random rnd = ThreadLocalRandom.current();
 		double lastDimSum = 0.0;
@@ -97,7 +103,7 @@ public final class DDP {
 		}
 		
 		// observed distributions sum
-		_observed = new DenseTensor(shape);
+		_obs = new DenseTensor(shape);
 		
 		// observed mass shape
 		final Shape obsMassShape;
@@ -109,39 +115,43 @@ public final class DDP {
 		}
 		
 		// observed mass tensor
-		_observedMass = new DenseTensor(obsMassShape);
+		_obsMass = new DenseTensor(obsMassShape);
 	}
 	
 	/**
 	 * Add observation of the provided discrete distribution 
 	 * with specified mass at the specified sub-location.
+	 * 
+	 * WARNING: the obsData must be the data of a
+	 * tensor with the LAST dimension to sample.
 	 */
-	public void addObservation(final Location subLoc, final Tensor dist, final double mass) {
+	public void addObservation(
+			final double mass,
+			final Location subLoc,
+			final double[] obsData,
+			int obsDataIndex) {
 
-		if (dist.size != _lastDimSize) {
-			throw new IllegalArgumentException(
-				"Observed distribution must be of size " + _lastDimSize + 
-				", but got the shape " + dist.shape + " of size " + dist.size);
+		// handle root
+		if (_ndim == 1) {
+			_obsMass.add(0, mass);
+		} else {
+			_obsMass.add(subLoc, mass);
 		}
+
+		// calculate write start index
+		final int [] writeIndices = Arrays.copyOf(subLoc.indices, _ndim);
+		final Location writeLocation = new Location(writeIndices);
+		int writeIndex = _shape.calcFlatIndexFromLocation(writeLocation);
 		
-		_observedMass.add(subLoc, mass);
-		
-		// TODO: root case (null subLoc)
-		// TODO: performance improvement
-		final int[] startIndices = new int[_ndim];
-		for (int i=0; i<_lastDim; i++) {
-			startIndices[i] = subLoc.indices[i];
-		}
-		final Location start = new Location(startIndices);
-		final int[] sizes = new int[_ndim];
-		for (int i=0; i<_lastDim; i++) {
-			sizes[i] = 1;
-		}
-		sizes[_lastDim] = _lastDimSize;
-		final Shape shape = new Shape(sizes);
-		final Tensor view = new ViewTensor(_observed, start, shape);
-		for (int i=0; i<view.size; i++) {
-			view.add(i, dist.get(i) * mass);
+		// write the data, crash on out of bounds if passed data is bad
+		for (int i=0; i<_lastDimSize; i++) {
+			
+			_obs.add(
+					writeIndex, 
+					mass*obsData[obsDataIndex]);
+
+			++obsDataIndex;
+			++writeIndex;
 		}
 	}
 	
@@ -149,19 +159,89 @@ public final class DDP {
 	 * Log-likelihood of the provided discrete
 	 * distribution under the posterior of this 
 	 * Discrete Dirichlet Process at the 
-	 * specified sub-location.
+	 * specified sub-location:
+	 * 
+	 * log(DP(a+n, a/(a+n)*H + n/(a+n)*Sample)),
+	 * 
+	 * DP(a, M) = Dir(a*M(x1), ..., a*M(xK)).
+	 * 
+	 * WARNING: the obsData must be the data of a
+	 * tensor with the LAST dimension to sample.
 	 */
-	public double posteriorLogLike(final Location subLoc, final Tensor dist) {
+	public double posteriorLogLike(
+			final Location subLoc,
+			final double[] obsData,
+			int obsDataIndex) {
 
-		if (dist.size != _lastDimSize) {
-			throw new IllegalArgumentException(
-				"Observed distribution must be of size " + _lastDimSize + 
-				", but got the shape " + dist.shape + " of size " + dist.size);
+		// handle root
+		final double obsMass; 
+		if (_ndim == 1) {
+			obsMass = _obsMass.get(0);
+		} else {
+			obsMass = _obsMass.get(subLoc);
 		}
-
-		// TODO
 		
-		return Double.NaN;
+		// observations threshold
+		final double adjMultiplier;
+		final double adjObsMass;
+		if (obsMass > _maxObsMass) {
+
+			// discount observed sums by overshoot
+			adjMultiplier = _maxObsMass / obsMass;
+			adjObsMass = _maxObsMass;
+			
+		} else {
+			
+			adjMultiplier = 1.0;
+			adjObsMass = obsMass;
+		}
+		
+		// calculate read start index
+		final int [] readIndices = Arrays.copyOf(subLoc.indices, _ndim);
+		final Location readLocation = new Location(readIndices);
+		int readIndex = _shape.calcFlatIndexFromLocation(readLocation);
+		
+		// calculate posterior dirichlet alpha
+		final double a = _baseMass;
+		final double n = adjObsMass;
+		final double a_plus_n = a + n;
+		final double posterior_dp_alpha = a_plus_n;
+		
+		// accumulate log-likelihood
+		double logLike = 0.0;
+
+		// sum up posterior dirichlet alphas
+		double sumPosteriorDirAlpha = 0.0;
+		
+		// read the data, crash on out of bounds if passed data is bad
+		for (int i=0; i<_lastDimSize; i++) {
+			
+			// read the values at index
+			final double baseIdxMass = _base.get(readIndex);
+			final double adjObsIdxMass = adjMultiplier*_obs.get(readIndex);
+			final double obsValue = obsData[obsDataIndex];
+			
+			final double posterior_dp_mass =
+					a / a_plus_n * baseIdxMass +
+					n / a_plus_n * adjObsIdxMass;
+			
+			final double posteriorDirAlpha =
+					posterior_dp_alpha *
+					posterior_dp_mass;
+			
+			// accumulate results
+			logLike -= GammaFunction.lnGamma(posteriorDirAlpha);
+			logLike += (posteriorDirAlpha - 1.0) * Math.log(obsValue);
+			sumPosteriorDirAlpha += posteriorDirAlpha;
+			
+			++obsDataIndex;
+			++readIndex;
+		}
+		
+		// finish off the normalization constant
+		logLike += GammaFunction.lnGamma(sumPosteriorDirAlpha);
+		
+		return logLike;
 	}
 
 }
