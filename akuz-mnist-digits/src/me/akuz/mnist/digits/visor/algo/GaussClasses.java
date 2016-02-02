994ec381e6f24fe1c94aa603f3507d3e6bc896a0
A Kuz <for.akuz@gmail.com>
2016-02-02 21:19:44 +0000

    Better verion of GoussClasses.

--- akuz-mnist-digits/src/me/akuz/mnist/digits/visor/algo/GaussClasses.java
+++ akuz-mnist-digits/src/me/akuz/mnist/digits/visor/algo/GaussClasses.java
@@ -22,80 +22,92 @@ public final class GaussClasses {
 		OUTER
 	}
 	
-	public static final int STAT_SUM = 0;
-	public static final int STAT_SUM_SQ = 1;
-	public static final int STATS_COUNT = 2;
+	public static final int PRIOR_STAT_MEAN = 0;
+	public static final int PRIOR_STAT_NYU = 1;
+	public static final int PRIOR_STAT_ALPHA = 2;
+	public static final int PRIOR_STAT_BETA = 3;
+	public static final int LENGTH_PRIOR_STAT = 4;
 	
-	private final int _outputClassCount;
-	private final int _inputChannelCount;
+	public static final int ADDED_STAT_SUM = 0;
+	public static final int ADDED_STAT_SUM_SQ = 1;
+	public static final int ADDED_STAT_WEIGHT = 2;
+	public static final int LENGTH_ADDED_STAT = 3;
+
+	private final int _classCount;
+	private final int _channelCount;
 	
-	private final Shape  _classShape;
-	private final Tensor _classCountPrior;
-	private final Tensor _classCountAdded;
+	private final Tensor _classPrior;
+	private final Tensor _classAdded;
 	
-	private final Shape  _classStatsShape;
-	private final Tensor _classStatsPrior;
-	private final Tensor _classStatsAdded;
+	private final Tensor _classPriorStats;
+	private final Tensor _classAddedStats;
 	
 	public GaussClasses(
-			final int outputClassCount,
-			final int inputChannelCount) {
+			final int classCount,
+			final int channelCount) {
 		
-		if (outputClassCount < 2) {
+		if (classCount < 2) {
 			throw new IllegalArgumentException(
-					"outputClassCount must be >= 2, got " + 
-					outputClassCount);
+					"classCount must be >= 2, got " + 
+					classCount);
 		}
-		_outputClassCount = outputClassCount;
+		_classCount = classCount;
 		
-		if (inputChannelCount < 1) {
+		if (channelCount < 1) {
 			throw new IllegalArgumentException(
-					"inputChannelCount must be >= 1, got " + 
-					inputChannelCount);
+					"channelCount must be >= 1, got " + 
+					channelCount);
 		}
-		_inputChannelCount = inputChannelCount;
+		_channelCount = channelCount;
 		
-		_classShape = new Shape(_outputClassCount);
-		_classStatsShape = new Shape(_outputClassCount, _inputChannelCount, STATS_COUNT);
-
-		_classCountPrior = new Tensor(_classShape);
-		_classCountAdded = new Tensor(_classShape);
+		_classPrior = new Tensor(new Shape(_classCount));
+		_classAdded = new Tensor(new Shape(_classCount));
 		
-		_classStatsPrior = new Tensor(_classStatsShape);
-		_classStatsAdded = new Tensor(_classStatsShape);
+		_classPriorStats = new Tensor(new Shape(_classCount, _channelCount, LENGTH_PRIOR_STAT));
+		_classAddedStats = new Tensor(new Shape(_classCount, _channelCount, LENGTH_ADDED_STAT));
 		
 		// set class and class stats priors
 		final Random rnd = ThreadLocalRandom.current();
-		final int[] statsIndices = new int[_classStatsShape.ndim];
-		final Location statsLoc = new Location(statsIndices);
-		for (int classIdx=0; classIdx<_outputClassCount; classIdx++) {
-			statsIndices[0] = classIdx;
+		final int[] priorStatsIndices = new int[_classPriorStats.ndim];
+		final Location priorStatsLoc = new Location(priorStatsIndices);
+		for (int classIdx=0; classIdx<_classCount; classIdx++) {
+			priorStatsIndices[0] = classIdx;
 			
 			// TODO from arguments
-			final double priorSamples = 1.0; 
-			_classCountPrior.set(classIdx, priorSamples);
+			final double classPriorSamples = 1.0;
+			_classPrior.set(classIdx, classPriorSamples);
 
-			for (int channelIdx=0; channelIdx<_inputChannelCount; channelIdx++) {
-				statsIndices[1] = channelIdx;
-				final int statsIdx = _classStatsShape.calcFlatIndexFromLocation(statsLoc);
-				
+			for (int channelIdx=0; channelIdx<_channelCount; channelIdx++) {
+				priorStatsIndices[1] = channelIdx;
+				final int priorStatsIdx = _classPriorStats.shape
+						.calcFlatIndexFromLocation(priorStatsLoc);
+
 				// TODO from arguments
 				final double priorMean = 0.5 + rnd.nextDouble()*0.01;
-				_classStatsPrior.set(statsIdx + STAT_SUM, 
-						priorSamples*priorMean);
+				final double priorMeanSamples = 1.0;
+				_classPriorStats.set(
+						priorStatsIdx + PRIOR_STAT_MEAN, 
+						priorMean);
+				_classPriorStats.set(
+						priorStatsIdx + PRIOR_STAT_NYU, 
+						priorMeanSamples);
 
 				// TODO from arguments
-				final double priorSigma = 1.0;
-				_classStatsPrior.set(statsIdx + STAT_SUM_SQ, 
-						priorSamples*Math.pow(priorSigma, 2) +
-						priorSamples*Math.pow(priorMean, 2));
+				final double priorVariance = Math.pow(1.0, 2);
+				final double priorVarianceSamples = 1.0;
+				_classPriorStats.set(
+						priorStatsIdx + PRIOR_STAT_ALPHA, 
+						priorVarianceSamples/2.0);
+				_classPriorStats.set(
+						priorStatsIdx + PRIOR_STAT_BETA,
+						priorVariance*priorVarianceSamples/2.0);
 			}
 		}
 	}
 	
 	public void clearObservations() {
-		_classCountAdded.fill(0.0);
-		_classStatsAdded.fill(0.0);
+		_classAdded.fill(0.0);
+		_classAddedStats.fill(0.0);
 	}
 	
 	public void addObservation(
@@ -111,29 +123,34 @@ public final class GaussClasses {
 			throw new NullPointerException("channelData");
 		}
 		
-		// write location
-		final int[] statsIndices = new int[_classStatsShape.ndim];
-		final Location statsLocation = new Location(statsIndices);
+		final Shape addedStatsShape = _classAddedStats.shape;
+		final int[] addedStatsIndices = new int[addedStatsShape.ndim];
+		final Location addedStatsLoc = new Location(addedStatsIndices);
 		
-		for (int classIdx = 0; classIdx < _outputClassCount; classIdx++) {
+		for (int classIdx=0; classIdx<_classCount; classIdx++) {
 			
-			statsIndices[0] = classIdx;
+			addedStatsIndices[0] = classIdx;
+
+			// get provided class probability
 			final double classProb = classProbs[classProbsStart + classIdx];
 			
 			// add to class counts
-			_classCountAdded.add(classIdx, classProb);
+			_classAdded.add(classIdx, classProb);
 			
-			for (int channelIdx = 0; channelIdx < _inputChannelCount; channelIdx++) {
+			for (int channelIdx=0; channelIdx<_channelCount; channelIdx++) {
 				
-				statsIndices[1] = channelIdx;
+				addedStatsIndices[1] = channelIdx;
+
+				// get provided channel value
 				final double channelValue = channelData[channelDataStart + channelIdx];
 				
 				// calculate stats index
-				final int statsStartIdx = _classStatsShape.calcFlatIndexFromLocation(statsLocation);
-	
+				final int startIdx = addedStatsShape.calcFlatIndexFromLocation(addedStatsLoc);
+				
 				// add to class stats
-				_classStatsAdded.add(statsStartIdx + STAT_SUM, classProb*channelValue);
-				_classStatsAdded.add(statsStartIdx + STAT_SUM_SQ, classProb*channelValue*channelValue);
+				_classAddedStats.add(startIdx + ADDED_STAT_SUM, classProb*channelValue);
+				_classAddedStats.add(startIdx + ADDED_STAT_SUM_SQ, classProb*channelValue*channelValue);
+				_classAddedStats.add(startIdx + ADDED_STAT_WEIGHT, classProb);
 			}
 		}
 	}
@@ -143,76 +160,88 @@ public final class GaussClasses {
 			final int classProbsStart,
 			final double[] channelData,
 			final int channelDataStart) {
+
+		if (classProbs == null) {
+			throw new NullPointerException("classProbs");
+		}
+		if (channelData == null) {
+			throw new NullPointerException("channelData");
+		}
 		
-		// write location
-		final int[] statsIndices = new int[_classStatsShape.ndim];
-		final Location statsLocation = new Location(statsIndices);
+		final Shape priorStatsShape = _classPriorStats.shape;
+		final int[] priorStatsIndices = new int[priorStatsShape.ndim];
+		final Location priorStatsLoc = new Location(priorStatsIndices);
+		
+		final Shape addedStatsShape = _classAddedStats.shape;
+		final int[] addedStatsIndices = new int[addedStatsShape.ndim];
+		final Location addedStatsLoc = new Location(addedStatsIndices);
 		
 		// calculate posterior log likelihood
-		for (int classIdx = 0; classIdx < _outputClassCount; classIdx++) {
+		for (int classIdx = 0; classIdx < _classCount; classIdx++) {
 			
-			statsIndices[0] = classIdx;
-				
-			// init with log of the output
-			classProbs[classProbsStart + classIdx] = 
-					StatsUtils.checkFinite(
-							Math.log(LOG_INSURANCE + classProbs[classProbsStart + classIdx]));
-
-			final double priorSamples = _classCountPrior.get(classIdx);
-			final double addedSamples = _classCountAdded.get(classIdx);
+			priorStatsIndices[0] = classIdx;
+			addedStatsIndices[0] = classIdx;
 			
-			// calculate prior nyu
-			final double priorNyu = priorSamples;
-
-			// calculate posterior nyu
-			final double posteriorNyu = priorNyu + addedSamples;
+			// start accumulating from zero
+			classProbs[classProbsStart + classIdx] = 0.0;
+			
+			// add likelihood from the arguments
+			{
+				classProbs[classProbsStart + classIdx] += 
+						StatsUtils.checkFinite(
+								Math.log(LOG_INSURANCE 
+										+ classProbs[classProbsStart + classIdx]));
+			}
+			
+			// add likelihood from class observations
+			{
+				final double priorSamples = _classPrior.get(classIdx);
+				final double addedSamples = _classAdded.get(classIdx);
+				classProbs[classProbsStart + classIdx] += 
+						StatsUtils.checkFinite(
+								Math.log(LOG_INSURANCE 
+										+ priorSamples
+										+ addedSamples));
+			}
 			
 			// add class stats likelihoods
-			for (int channelIdx = 0; channelIdx < _inputChannelCount; channelIdx++) {
+			for (int channelIdx = 0; channelIdx < _channelCount; channelIdx++) {
 				
-				statsIndices[1] = channelIdx;
-				final double channelValue = channelData[channelDataStart + channelIdx];
+				priorStatsIndices[1] = channelIdx;
+				addedStatsIndices[1] = channelIdx;
 				
-				// calculate stats index
-				final int statsStartIdx = _classStatsShape.calcFlatIndexFromLocation(statsLocation);
+				final int priorStartIdx = priorStatsShape.calcFlatIndexFromLocation(priorStatsLoc);
+				final int addedStartIdx = addedStatsShape.calcFlatIndexFromLocation(addedStatsLoc);
 				
-				// get accumulated stats
-				final double priorSum = _classStatsPrior.get(statsStartIdx + STAT_SUM);
-				final double priorSumSq = _classStatsPrior.get(statsStartIdx + STAT_SUM_SQ);
-				
-				// get accumulated stats
-				final double addedSum = _classStatsAdded.get(statsStartIdx + STAT_SUM);
-				final double addedSumSq = _classStatsAdded.get(statsStartIdx + STAT_SUM_SQ);
-				
-				// calculate prior mean
-				final double priorMean = 
-						priorSum /
-						priorSamples;
+				final double priorMean = _classPriorStats.get(priorStartIdx + PRIOR_STAT_MEAN);
+				final double priorNyu = _classPriorStats.get(priorStartIdx + PRIOR_STAT_NYU);
+				final double priorAlpha = _classPriorStats.get(priorStartIdx + PRIOR_STAT_ALPHA);
+				final double priorBeta = _classPriorStats.get(priorStartIdx + PRIOR_STAT_BETA);
+
+				final double addedSum = _classAddedStats.get(addedStartIdx + ADDED_STAT_SUM);
+				final double addedSumSq = _classAddedStats.get(addedStartIdx + ADDED_STAT_SUM_SQ);
+				final double addedWeight = _classAddedStats.get(addedStartIdx + ADDED_STAT_WEIGHT);
+
+				// calculate posterior nyu
+				final double posteriorNyu = 
+						priorNyu + addedWeight;
 				
 				// calculate posterior mean
 				final double posteriorMean = 
-						(priorSum + addedSum) /
-						(priorSamples + addedSamples);
-
-				// calculate prior alpha
-				final double priorAlpha = priorSamples / 2.0;
+						(priorNyu*priorMean + addedSum) /
+						(priorNyu + addedWeight);
 
 				// calculate posterior alpha
-				final double posteriorAlpha = priorAlpha + addedSamples / 2.0;
-
-				// calculate prior beta
-				final double priorBeta = (
-						priorSumSq - 
-						priorSamples * Math.pow(priorMean, 2)
-						) / 2.0;
+				final double posteriorAlpha = 
+						priorAlpha + addedWeight / 2.0;
 
 				// calculate posterior beta
 				final double posteriorBeta;
-				if (addedSamples > 0.0) {
+				if (addedWeight > 0.0) {
 					posteriorBeta = priorBeta 
-							+ 0.5 * (addedSumSq - addedSamples*Math.pow(addedSum/addedSamples, 2))
-							+ 0.5 * (priorNyu*addedSamples) / (priorNyu + addedSamples) 
-							* Math.pow(addedSum/addedSamples - priorMean, 2);
+							+ 0.5 * (addedSumSq - Math.pow(addedSum, 2)/addedWeight)
+							+ 0.5 * (priorNyu*addedWeight) / (priorNyu + addedWeight) 
+							* Math.pow(addedSum/addedWeight - priorMean, 2);
 				} else {
 					posteriorBeta = priorBeta;
 				}
@@ -225,6 +254,9 @@ public final class GaussClasses {
 						/ posteriorAlpha 
 						/ posteriorNyu;
 				
+				// get provided channel value
+				final double channelValue = channelData[channelDataStart + channelIdx];
+
 				// Student-t log like of channelValue
 				classProbs[classProbsStart + classIdx] += 
 						StatsUtils.checkFinite(
@@ -240,7 +272,7 @@ public final class GaussClasses {
 		StatsUtils.logLikesToProbsInPlace(
 				classProbs, 
 				classProbsStart,
-				_outputClassCount);
+				_classCount);
 	}
 	
 	public void calculateChannelMeans(
@@ -256,40 +288,49 @@ public final class GaussClasses {
 			throw new NullPointerException("channelData");
 		}
 		
-		// write location
-		final int[] statsIndices = new int[_classStatsShape.ndim];
-		final Location statsLocation = new Location(statsIndices);
+		final Shape priorStatsShape = _classPriorStats.shape;
+		final int[] priorStatsIndices = new int[priorStatsShape.ndim];
+		final Location priorStatsLoc = new Location(priorStatsIndices);
 		
+		final Shape addedStatsShape = _classAddedStats.shape;
+		final int[] addedStatsIndices = new int[addedStatsShape.ndim];
+		final Location addedStatsLoc = new Location(addedStatsIndices);
+
 		// first reset means to zero
-		for (int channelIdx = 0; channelIdx < _inputChannelCount; channelIdx++) {
+		for (int channelIdx = 0; channelIdx < _channelCount; channelIdx++) {
 			channelData[channelDataStart + channelIdx] = 0.0;
 		}
 		
-		// populate means using class probabilities
-		for (int classIdx = 0; classIdx < _outputClassCount; classIdx++) {
+		// calculate posterior log likelihood
+		for (int classIdx = 0; classIdx < _classCount; classIdx++) {
 			
-			statsIndices[0] = classIdx;
+			priorStatsIndices[0] = classIdx;
+			addedStatsIndices[0] = classIdx;
+			
+			// get provided class probability
 			final double classProb = classProbs[classProbsStart + classIdx];
-			
-			final double priorSamples = _classCountPrior.get(classIdx);
-			final double addedSamples = _classCountAdded.get(classIdx);
 
-			for (int channelIdx = 0; channelIdx < _inputChannelCount; channelIdx++) {
+			// add class stats likelihoods
+			for (int channelIdx = 0; channelIdx < _channelCount; channelIdx++) {
 				
-				statsIndices[1] = channelIdx;
+				priorStatsIndices[1] = channelIdx;
+				addedStatsIndices[1] = channelIdx;
 				
-				// calculate stats index
-				final int statsStartIdx = _classStatsShape.calcFlatIndexFromLocation(statsLocation);
+				final int priorStartIdx = priorStatsShape.calcFlatIndexFromLocation(priorStatsLoc);
+				final int addedStartIdx = addedStatsShape.calcFlatIndexFromLocation(addedStatsLoc);
 				
-				// get sum statistics
-				final double priorSum = _classStatsPrior.get(statsStartIdx + STAT_SUM);
-				final double addedSum = _classStatsAdded.get(statsStartIdx + STAT_SUM);
+				final double priorMean = _classPriorStats.get(priorStartIdx + PRIOR_STAT_MEAN);
+				final double priorNyu = _classPriorStats.get(priorStartIdx + PRIOR_STAT_NYU);
+
+				final double addedSum = _classAddedStats.get(addedStartIdx + ADDED_STAT_SUM);
+				final double addedWeight = _classAddedStats.get(addedStartIdx + ADDED_STAT_WEIGHT);
 				
 				// calculate posterior mean
 				final double posteriorMean = 
-						(priorSum + addedSum) /
-						(priorSamples + addedSamples);
-				
+						(priorNyu*priorMean + addedSum) /
+						(priorNyu + addedWeight);
+
+				// add to the output channel data
 				channelData[channelDataStart + channelIdx] 
 						+= classProb*posteriorMean;
 			}
